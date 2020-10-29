b4f3868299c5d78417d2f8578423f91619cf86d5
fernando.ruffino <fernando.ruffino@scanbrazilconsulting.com>
2020-10-29 17:58:45 -0300

    alterações memoryDatabase

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -5,17 +5,15 @@ import { Entry } from "./pages/entries/shared/entry.model";
 export class InMemoryDatabase implements InMemoryDbService {
   createDb(){
     const categories: Category[] = [
-      { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
-      { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
-      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' }
+      { id: 1, name: 'Saúde', description: 'Remédios e produtos de beleza' },
+      { id: 2, name: 'Alimentação', description: 'Processados, frutas e verduras' },
+      { id: 3, name: 'Terceiros', description: 'Otimização da empresa' }
     ];
 
     const entries: Entry[] = [
-      { id: 1, name: 'Gás de Cozinha', categoryId: categories[0].id, category: categories[0], paid: true, date: "14/10/2018", amount: "70,80", type: "expense", description: "Qualquer descrição para essa despesa" } as Entry,
-      { id: 2, name: 'Salário na Empresa X', categoryId: categories[1].id, category: categories[1], paid: true, date: "15/10/2018", amount: "4405,49", type: "revenue" } as Entry,
-      { id: 3, name: 'Uber', categoryId: categories[1].id, category: categories[1], paid: true, date: "17/10/2018", amount: "30,00", type: "expense" } as Entry,
-      { id: 4, name: 'Aluguel', categoryId: categories[2].id, category: categories[2], paid: false, date: "23/10/2018", amount: "15,00", type: "expense" } as Entry,
-      { id: 5, name: 'Pagamento Pelo Projeto XYZ', categoryId: categories[2].id, category: categories[2], paid: true, date: "25/10/2018", amount: "2980,00", type: "revenue" } as Entry
+      { id: 1, name: 'Almoço Restaurante B', categoryId: categories[1].id, category: categories[1], paid: true, date: "28/10/2020", amount: "45,90", type: "revenue" } as Entry,
+      { id: 2, name: 'Remédio A', categoryId: categories[0].id, category: categories[0], paid: true, date: "29/10/2020", amount: "15,00", type: "revenue" } as Entry,
+      { id: 3, name: 'Projeto XYZ', categoryId: categories[2].id, category: categories[2], paid: false, date: "25/11/2020", amount: "2850,00", type: "expense" } as Entry
     ];
 
     return { categories, entries }
